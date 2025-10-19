7149803186d46be73990a7adf2d51f307322a739
Sammuel-0912 <sam60320@gmail.com>
2025-10-19 13:14:35 +0800

    add files vias upload
    change data source via axios

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
 let data = [
-    {
-      "id": 0,
-      "name": "肥宅心碎賞櫻3日",
-      "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-      "area": "高雄",
-      "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-      "group": 87,
-      "price": 1400,
-      "rate": 10
-    },
-    {
-      "id": 1,
-      "name": "貓空纜車雙程票",
-      "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台北",
-      "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-      "group": 99,
-      "price": 240,
-      "rate": 2
-    },
-    {
-      "id": 2,
-      "name": "台中谷關溫泉會1日",
-      "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台中",
-      "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-      "group": 20,
-      "price": 1765,
-      "rate": 7
-    }
+   
   ];
+const url = "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+
 const ticketCardArea = document.querySelector('.ticketCard-area'); 
 const regionSearch = document.querySelector(".regionSearch"); 
 const addTicketBtn = document.querySelector(".addTicket-Btn"); 
@@ -63,7 +36,7 @@ function render(dataList) {
   // searchResultText.textContent = `本次搜尋共 ${ticketData.length} 筆資料`;
 
 }
-render(data); 
+// render(data); 
 
 //監聽篩選下拉選單
 regionSearch.addEventListener("change",function(e) {
@@ -112,3 +85,20 @@ addTicketBtn.addEventListener('click',function(e) {
   document.querySelector(".addTicket-form").reset();
 });
 
+async function getData() {
+  const res = await axios.get(url); 
+  data = res.data.data ; 
+  render(data); 
+}
+
+// function getData() {
+//   axios.get(url)
+//   .then(function(res) {
+//    data = res.data.data;
+//    render(data); 
+//   })
+//   .catch(function() {
+//     console.log("發生錯誤"); 
+//   }); 
+// }
+getData(); 
